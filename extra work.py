9431eec99983365187695ba661ad90c6a85b902c
Sandeep-svg268 <116118734+Sandeep-svg268@users.noreply.github.com>
2022-12-06 21:38:08 -0600

    Add files via upload

---
+++ extra work.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy as np
+import math
+
+
+# In[ ]:
+
+
+width of the channel = 600ft
+flow of maximum = 75
+I = 0.020
+S = 3%
+Angle = 45
+L = 1
+Q = 15000cfs
+Q = 1.49/n*A*R*2/3**S
+15000 = 1.49/
+
